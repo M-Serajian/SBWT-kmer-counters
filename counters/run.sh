7ba3f2f66ca0bd71fc261ab877a9a20fbc3e7458
Jarno N. Alanko <alanko.jarno@gmail.com>
2022-11-19 01:15:38 +0200

    Fix script

--- counters/run.sh
+++ counters/run.sh
@@ -4,7 +4,7 @@ set -xue
 find genomes -type f > in_file_list.txt
 
 # Build the SBWT index
-../build/bin/sbwt build --in-file in_file_list.txt -k 31 -out index.sbwt -t 8 -m 10 --temp-dir temp
+../build/bin/sbwt build --in-file in_file_list.txt -k 31 -o index.sbwt -t 8 -m 10 --temp-dir temp
 
 # Build the counters
 ./counters ../index.sbwt genomes/*.fna
